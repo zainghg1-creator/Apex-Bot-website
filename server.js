ea3969d470fbf214a3e8f9304e09f88d239ef54b
zainghg1-creator <zainghg1@gmail.com>
2026-07-26 11:08:42 +0200

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ console.log('REDIRECT_URI:', REDIRECT_URI);
 
 const DISCORD_API = 'https://discord.com/api/v10';
 const ADMINISTRATOR = 0x8n;
-const ALLOWED_MODULES = ['welcome', 'tickets', 'teamliste', 'support', 'moderation', 'teamupdate', 'stats', 'verification', 'antinuke'];
+const ALLOWED_MODULES = ['welcome', 'tickets', 'teamliste', 'support', 'moderation', 'teamupdate', 'stats', 'verification', 'antinuke', 'minigames'];
 
 // ============================================================
 // EXPRESS APP
